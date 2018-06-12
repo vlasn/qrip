e77132ac7ce430555f0044be443fcc2ae8d19cb9
Veljo Lasn <needikola@gmail.com>
2018-06-13 01:07:55 +0300

    fixed --help

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const { opts, params } = args.reduce((a, arg) =>{
     return a
 }, {opts: [], params: []})
 
-
-if (params.length < 1) {
-    console.error('Please specify port or run `qrip --h` for help')
-} else if (opts.includes('--h') || opts.includes('--help')) {
+if (opts.includes('--help')) {
     console.log(`
 Usage:
     * \`qrip 4000\` to simply generate a QR code with your local network address + the specified port (400 in this case)
@@ -21,6 +18,8 @@ Usage:
     * \`qrip 4000 /landing/hi --ngrok\` will open an ngrok tunnel and create a QR link referring to that instead. This deprecates the limitation of needing to have both devices on the same network.
 All other options you pass in will be lost in time, like tears in rain.
     `)
+} else if (params.length < 1) {
+    console.error('Please specify port or run `qrip --help` for help')
 } else {
 
     const printQR = async (port, path, ngrokEnabled) => {
